c5aa9b8849e3fd07dde4db489a2608873ecfdac9
Michael Yang <admin@my4ng.dev>
2024-11-05 15:16:11 +0000

    add download function

--- fynesse/access.py
+++ fynesse/access.py
@@ -7,6 +7,23 @@ import tables
 import mongodb
 import sqlite"""
 
+import requests
+
+def download_price_paid_data(year_from, year_to):
+    # Base URL where the dataset is stored
+    base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
+    """Download UK house price data for given year range"""
+    # File name with placeholders
+    file_name = "/pp-<year>-part<part>.csv"
+    for year in range(year_from, (year_to+1)):
+        print (f"Downloading data for year: {year}")
+        for part in range(1,3):
+            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
+                    file.write(response.content)
+
 # This file accesses the data
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
