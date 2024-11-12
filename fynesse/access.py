15897b081a37e738a61a2013f958224f2e177adb
Michael Yang <admin@my4ng.dev>
2024-11-12 15:06:31 +0000

    practical 2

--- fynesse/access.py
+++ fynesse/access.py
@@ -12,6 +12,10 @@ import requests
 import zipfile
 import pymysql
 import csv
+import sqlalchemy
+
+import osmnx as ox
+import pandas as pd
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
@@ -65,7 +69,7 @@ def create_connection(user, password, host, database, port=3306):
                                host=host,
                                port=port,
                                local_infile=1,
-                               db=database
+                               db=database,
                                )
         print(f"Connection established!")
     except Exception as e:
@@ -77,9 +81,15 @@ def housing_upload_join_data(data_dir_path, conn, year):
         start_date = str(year) + "-01-01"
         end_date = str(year) + "-12-31"
 
-        cur = conn.cursor()
         print('Selecting data for year: ' + str(year))
-        cur.execute(f'SELECT pp.price, pp.date_of_transfer, po.postcode, pp.property_type, pp.new_build_flag, pp.tenure_type, pp.locality, pp.town_city, pp.district, pp.county, po.country, po.latitude, po.longitude FROM (SELECT price, date_of_transfer, postcode, property_type, new_build_flag, tenure_type, locality, town_city, district, county FROM pp_data WHERE date_of_transfer BETWEEN "' + start_date + '" AND "' + end_date + '") AS pp INNER JOIN postcode_data AS po ON pp.postcode = po.postcode')
+        cur.execute(f"""
+                    SELECT pp.*, po.country, po.latitude, po.longitude FROM (SELECT price, 
+                    date_of_transfer, postcode, property_type, new_build_flag, tenure_type, 
+                    primary_addressable_object_name, secondary_addressable_object_name, 
+                    street, locality, town_city, district, county FROM pp_data 
+                    WHERE date_of_transfer BETWEEN "{start_date}" AND "{end_date}") AS pp 
+                    INNER JOIN postcode_data AS po ON pp.postcode = po.postcode
+                    """)
         rows = cur.fetchall()
 
         csv_file_path = data_dir_path + '/output_file.csv'
@@ -96,6 +106,75 @@ def housing_upload_join_data(data_dir_path, conn, year):
         os.remove(csv_file_path)
     conn.commit()
 
+def pois_near_coordinates_df(latitude: float, longitude: float, features: dict, distance_km: float = 1.0) -> pd.DataFrame:
+    """
+    Count Points of Interest (POIs) near a given pair of coordinates within a specified distance.
+    Args:
+        latitude (float): Latitude of the location.
+        longitude (float): Longitude of the location.
+        features (dict): A dictionary of interested features and its matching tag-value list.
+        distance_km (float): The distance around the location in kilometers. Default is 1 km.
+    Returns:
+        pd: A pandas DataFrame that holds the results of the
+    """
+    tags = { tag[0]: True for feature in features.values() for tag in feature }
+    pois = ox.features_from_point((latitude, longitude), tags, distance_km * 1000.)
+    return pd.DataFrame(pois)
+
+def count_pois_near_coordinates(latitude: float, longitude: float, features: dict, distance_km: float = 1.0) -> dict:
+    """
+    Count Points of Interest (POIs) near a given pair of coordinates within a specified distance.
+    Args:
+        latitude (float): Latitude of the location.
+        longitude (float): Longitude of the location.
+        features (dict): A dictionary of interested features and its matching tag-value list.
+        distance_km (float): The distance around the location in kilometers. Default is 1 km.
+    Returns:
+        dict: A dictionary where keys are the OSM tags and values are the counts of POIs for each tag.
+    """
+    pois = pois_near_coordinates_df(latitude, longitude, features, distance_km)
+    poi_counts = {}
+
+    for name, feature in features.items():
+        pred = False
+        for tag in feature:
+            if tag[1] is True:
+                pred |= (pois[tag[0]].notna())
+            else:
+                for value in tag[1]:
+                    pred |= (pois[tag[0]] == value)
+        poi_counts[name] = pred.sum()
+    return poi_counts
+
+def full_addr_buildings_gdf(latitude: float, longitude: float, size: float) -> pd.DataFrame:
+    bbox = ox.utils_geo.bbox_from_point((latitude, longitude), size * 500)
+    gdf = ox.features_from_bbox(bbox, { 'building': True })
+    gdf['joined'] = (gdf['addr:housenumber'].fillna(gdf['addr:housename']) + ' ' 
+                            + gdf['addr:street']).fillna(gdf['name']).str.upper()
+    gdf['area'] = gdf['geometry'].to_crs(epsg=27700).area
+    return gdf
+
+def pp_transactions_df(latitude: float, longitude: float, size: float, conn: pymysql.Connection, 
+                       start_date: str, end_date: str | None = None) -> pd.DataFrame:
+    bbox = ox.utils_geo.bbox_from_point((latitude, longitude), size * 500)
+    left, bottom, right, top = bbox
+
+    if end_date is None:
+        query = f"""
+        SELECT * FROM `prices_coordinates_data` 
+        WHERE date_of_transfer >= '{start_date}'
+        AND latitude BETWEEN {bottom} AND {top}
+        AND longitude BETWEEN {left} AND {right}
+        """
+    else:
+        query = f"""
+        SELECT * FROM `prices_coordinates_data` 
+        WHERE date_of_transfer BETWEEN '{start_date}' AND '{end_date}'
+        AND latitude BETWEEN {bottom} AND {top}
+        AND longitude BETWEEN {left} AND {right}
+        """
+
+    return pd.read_sql(query, conn)
 
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
--- fynesse/assess.py
+++ fynesse/assess.py
@@ -12,6 +12,111 @@ import sklearn.feature_extraction"""
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
+import osmnx as ox
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+import numpy.polynomial.polynomial as polynomial
+
+def plot_full_addr_buildings(name: str, latitude: float, longitude: float, size: float, gdf: pd.DataFrame):
+    bbox = ox.utils_geo.bbox_from_point((latitude, longitude), size * 500)
+    left, bottom, right, top = bbox
+
+    graph = ox.graph_from_bbox(bbox, truncate_by_edge=True)
+    _, edges = ox.graph_to_gdfs(graph)
+
+    _, ax = plt.subplots(figsize=(8,8))
+
+    edges.plot(ax=ax, linewidth=1, edgecolor="dimgray")
+    color = gdf['joined'].notnull().map(lambda b: 'C0' if b else 'silver')
+    gdf.plot(ax=ax, color=color)
+
+    ax.set_title(f"{name}: Full Address Buildings")
+    ax.set_xlim([left, right])
+    ax.set_ylim([bottom, top])
+    ax.set_xlabel("longitude")
+    ax.set_ylabel("latitude")
+    ax.ticklabel_format(useOffset=False)
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.tight_layout()
+
+def plot_pp_transactions(name: str, latitude: float, longitude: float, size: float, df: pd.DataFrame):
+    bbox = ox.utils_geo.bbox_from_point((latitude, longitude), size * 500)
+    left, bottom, right, top = bbox
+    
+    graph = ox.graph_from_bbox(bbox, truncate_by_edge=True)
+    _, edges = ox.graph_to_gdfs(graph)
+
+    _, ax = plt.subplots(figsize=(8,8))
+
+    edges.plot(ax=ax, linewidth=1, edgecolor="dimgray")
+    ax.scatter(df['longitude'], df['latitude'], marker='.', s=50, alpha=0.25)
+    ax.set_title(f"{name}: Transactions")
+
+    ax.set_xlim([left, right])
+    ax.set_ylim([bottom, top])
+    ax.set_xlabel("longitude")
+    ax.set_ylabel("latitude")    
+    ax.ticklabel_format(useOffset=False)
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.tight_layout()
+
+def join_addr_pp_transaction(full_addr_df: pd.DataFrame, transaction_df: pd.DataFrame) -> pd.DataFrame:
+    transaction_df['joined'] = transaction_df['primary_addressable_object_name'] + ' ' + transaction_df['street']
+    joined_df = full_addr_df.merge(transaction_df, on='joined', how='left')
+    joined_df = joined_df[['date_of_transfer', 'price', 'joined', 'area', 'geometry']]
+    return joined_df
+
+def plot_joined_addr_pp_transaction(name: str, latitude: float, longitude: float, size: float, joined_df: pd.DataFrame):
+    bbox = ox.utils_geo.bbox_from_point((latitude, longitude), size * 500)
+    left, bottom, right, top = bbox
+    
+    graph = ox.graph_from_bbox(bbox, truncate_by_edge=True)
+    _, edges = ox.graph_to_gdfs(graph)
+
+    _, ax = plt.subplots(figsize=(8,8))
+
+    edges.plot(ax=ax, linewidth=1, edgecolor="dimgray")
+    column = joined_df['price'].notnull().astype(int) + joined_df['joined'].notnull().astype(int)
+    colors = ['silver', 'C0', 'C3']
+    color = column.apply(lambda i: colors[i])
+    joined_df.plot(ax=ax, color=color)
+    ax.set_title(f"{name}: Matched transaction buildings")
+
+    ax.set_xlim([left, right])
+    ax.set_ylim([bottom, top])
+    ax.set_xlabel("longitude")
+    ax.set_ylabel("latitude")    
+    ax.ticklabel_format(useOffset=False)
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.tight_layout()
+
+def visualise_area_price_corr(name: str, joined_df: pd.DataFrame):
+    _, ax = plt.subplots()
+
+    joined_df = joined_df[joined_df['price'].notnull()]
+    r, p = stats.pearsonr(joined_df['area'], joined_df['price'])
+    print(f"Pearson's r: {r:.3f}")
+
+    b, k = polynomial.polyfit(joined_df['area'], joined_df['price'], 1)
+    a = np.linspace(0, 1000, 100)
+
+    ax.scatter(joined_df['area'], joined_df['price'], s=10)
+
+    ax.set_title(f"{name}: Area/price correlation")
+    ax.plot(a, b + k * a, lw=1, color='k')
+    ax.ticklabel_format(useOffset=False, style='plain')
+    ax.set_xlim(0, 1000)
+    ax.set_ylim(0)
+    ax.set_xlabel("area")
+    ax.set_ylabel("price")
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.tight_layout()
 
 def data():
     """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
